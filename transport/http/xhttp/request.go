532233c378e02d80f385d3409a985b324c434c55
neo <neo532@126.com>
2023-10-12 15:48:36 +0800

    clean

--- transport/http/xhttp/request.go
+++ transport/http/xhttp/request.go
@@ -200,7 +200,7 @@ func (r *Request) log(c context.Context,
 	}
 
 	reqBodyS := string(reqBody)
-	if len(reqBodyS) != 0 {
+	if len(reqBodyS) != 0 && reqBodyS != "{}" {
 		reqBodyS = " -d '" + string(reqBodyS) + "'"
 	}
 	msg := fmt.Sprintf("[code:%d] [limit:%s] [cost:%s] [curl -X '%s' '%s'%s%s] [rst:%s]",
